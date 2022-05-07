883b90f1921dcdd1946d55fc1b1e17e40ce207e9
srikrishna uppili <srikrishnauppili@gmail.com>
2022-05-07 18:57:15 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { EmplistComponent } from './emplist/emplist.component';
+import { EmpaddComponent } from './empadd/empadd.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { StarComponent } from './shared/star.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmplistComponent,
+    EmpaddComponent,
+    AboutmeComponent,
+    StarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
